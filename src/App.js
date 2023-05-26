edf07434eeb4b93518b0a828e67e1abcf1ae516c
VijayarajThangadurai <124797617+VijayarajThangadurai@users.noreply.github.com>
2023-05-26 20:07:50 +0530

    loop task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import ExpenseItem from "./components/Expenseitem";
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      location: 'home',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', location: 'tv showroom', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      location:'car showroom',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      location: 'wooden stores',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div>
         <h1>lets get started!</h1>
-         <ExpenseItem></ExpenseItem>
+        {expenses.map((expenses, index) => (
+        <>
+        <div key= {expenses.id}> <ExpenseItem 
+         title ={expenses.title}
+         location={expenses.location}
+         amount={expenses.amount}
+         date={expenses.date}></ExpenseItem> </div> </>
+        
+        ))}
+      
+  
+        
     </div>
   );
 }
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,21 +1,14 @@
 import './Expenseitem.css';
-function ExpenseItem(){
-    const expenseDate = new Date(2023, 2,28);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount= "294.67";
-    const expenseLocation ="Car Showroom";
-
+function ExpenseItem(props){
     return ( 
           <div className="expense-item">
-            <div><h3>{expenseDate.toISOString()}</h3></div>
+            <div>{props.date.toISOString()}</div>
            <div className="expense-item__description">
-           <h2>{expenseTitle}</h2>
-           <div className="expense-item__location"><h2>{expenseLocation}</h2></div>
-           <div className="expense-item__price">${expenseAmount}</div>
-          
-             </div>
-             </div>
-             
+           <h2>{props.title}</h2>
+           <div className="expense-item__location"><h2>{props.location}</h2></div>
+           <div className="expense-item__price">${props.amount}</div>
+           </div>
+             </div> 
     );
 }
  export default ExpenseItem;
