dbb18418ededee21994a29405584ed4ae646304d
VijayarajThangadurai <124797617+VijayarajThangadurai@users.noreply.github.com>
2023-05-26 18:49:06 +0530

    task

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,14 +1,21 @@
+import './Expenseitem.css';
 function ExpenseItem(){
-    return ( <div> 
-        <div>
-           <div> 
-            <h3>Expense Items</h3>
-           </div>
-           <div>Food Rs.10</div> 
-           <div> Petrol Rs.100</div> 
-            <div>Movies Rs.200</div> 
-            </div>
-    </div>
+    const expenseDate = new Date(2023, 2,28);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount= "294.67";
+    const expenseLocation ="Car Showroom";
+
+    return ( 
+          <div className="expense-item">
+            <div><h3>{expenseDate.toISOString()}</h3></div>
+           <div className="expense-item__description">
+           <h2>{expenseTitle}</h2>
+           <div className="expense-item__location"><h2>{expenseLocation}</h2></div>
+           <div className="expense-item__price">${expenseAmount}</div>
+          
+             </div>
+             </div>
+             
     );
 }
  export default ExpenseItem;
