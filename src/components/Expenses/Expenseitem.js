5b9812dc94c84bb65ceb5c41404ffeeaddf73dd9
VijayarajThangadurai <124797617+VijayarajThangadurai@users.noreply.github.com>
2023-05-29 21:27:39 +0530

    manipulating the DOM

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState }from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails  from './ExpenseDetails';
 import Card from '../UI/Card';
 import './Expenseitem.css';
 const ExpenseItem=(props)=>{
+  const [title, setTitle] = useState(props.title);
+  const[amount, setAmount]= useState(props.amount);
+console.log('ExpenseItem evaluated by React');
   const ClickHandler = ()=>{
-    console.log("Clicked!!!..");
+    setTitle("Updated!");
+    console.log(title);
+  };
+  const UpdateExpense = ()=>{
+    setAmount("100");
+    console.log(amount);
   };
   const DeleteExpense =()=>{
     console.log("Deleted!!!..");
@@ -13,11 +21,12 @@ const ExpenseItem=(props)=>{
     return ( 
           <Card className="expense-item">
             <ExpenseDate date={props.date}/>
-           <ExpenseDetails title={props.title} 
+           <ExpenseDetails title={title} 
            location ={props.location} 
-           amount={props.amount}/>
+           amount={amount}/>
            <button onClick={ClickHandler}>Change Title</button>
            <button onClick={DeleteExpense}>Delete Expense</button>
+           <button onClick={UpdateExpense}>Change Price</button>
             </Card>
     
     );
