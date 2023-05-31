3e820ebab60ce29383fd4d8a994cedf35649f578
VijayarajThangadurai <124797617+VijayarajThangadurai@users.noreply.github.com>
2023-05-31 21:18:30 +0530

    lets introduce the  state inside the components task 4

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,14 +1,17 @@
-import React from'react';
+import React, {useState} from'react';
 import './ExpenseForm.css';
 const ExpenseForm = ()=>{
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enetredAmount, setEnteredAmount] = useState('');
+    const [enetredDate, setEntredDate] = useState('');
         const titleChangeHandler=(event)=>{
-          console.log(event.target.value);
+          setEnteredTitle(event.target.value);
         };
         const amountChangeHandler=(event)=>{
-            console.log(event.target.value);
+            setEnteredAmount(event.target.value);
           };
           const dateChangeHandler=(event)=>{
-            console.log(event.target.value);
+            setEntredDate(event.target.value);
           };
 return (
 <form>
