6f6b60384f32cb91e5d5dbf62e01ce975551ec4d
VijayarajThangadurai <124797617+VijayarajThangadurai@users.noreply.github.com>
2023-06-03 19:03:44 +0530

    task 11
    Implementing the filter feature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,38 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from "./Expenseitem";
 import ExpensesFilter from './ExpensesFilter';
 import Card from "../UI/Card";
 import './Expenses.css';
-const  Expenses = (props) =>{
- const [filteredYear, setFilteredYear]= useState('2020');
- const filterChangeHandler = selectedYear => {
-  setFilteredYear(selectedYear);
- };
+const Expenses = (props) => {
+    console.log(props.item, "IN Expenses");
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
- 
- return (
-  <div>
+  const filterExpenses = props.item.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear);
+
+
+  return (
+    <div>
       <Card className="expenses">
-        <ExpensesFilter 
-        selected = {filteredYear} 
-        onChangeFilter={filterChangeHandler}
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-        {props.item.map((expense) => (
-          <ExpenseItem 
-          key ={expense.id}
-          title={expense.title}
-          location={expense.location}
-          amount={expense.amount}
-          date={expense.date}
-         />
-         ))}
+        {filterExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            location={expense.location}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
-      </div>
-    );
+    </div>
+  );
 };
 
 export default Expenses;
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -7,7 +7,7 @@ const ExpenseFilter = (props) => {
    const dropDownChangeHandler = (event) => {
    props.onChangeFilter(event.target.value);
   };
-  console.log(props.selected);
+  // console.log(props.selected);
 
   return (
     <div className="expenses-filter">
